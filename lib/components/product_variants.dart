7e4c72fff1470d85adf5e82768b759396394975e
mdaffailhami <mdaffailhami@gmail.com>
2024-08-13 07:52:59 +0800

    Make selecting variants works

--- lib/components/product_variants.dart
+++ lib/components/product_variants.dart
@@ -2,11 +2,62 @@ import 'package:aksamedia_msib/widgets/choice_chip.dart';
 import 'package:aksamedia_msib/widgets/color_choice_chip.dart';
 import 'package:flutter/material.dart';
 
-class MyProductVariants extends StatelessWidget {
+class MyProductVariants extends StatefulWidget {
   const MyProductVariants({super.key});
 
   @override
+  State<MyProductVariants> createState() => _MyProductVariantsState();
+}
+
+class _MyProductVariantsState extends State<MyProductVariants> {
+  // State management with ValueNotifier
+  final _selectedSizeIndex = ValueNotifier<int>(0);
+  final _selectedColorIndex = ValueNotifier<int>(0);
+
+  final _sizes = ['Paket 1', 'Paket 2'];
+  final _colors = const [Color(0xFFDDB69A), Color(0xFF5B3E36)];
+
+  @override
   Widget build(BuildContext context) {
+    final sizeListWidget = ValueListenableBuilder(
+      valueListenable: _selectedSizeIndex,
+      builder: (context, value, _) {
+        return Row(
+          children: List.generate(_sizes.length, (i) {
+            return Padding(
+              padding: const EdgeInsets.only(right: 10),
+              child: MyChoiceChip(
+                selected: i == value,
+                text: _sizes[i],
+                onPressed: () {
+                  _selectedSizeIndex.value = i;
+                },
+              ),
+            );
+          }),
+        );
+      },
+    );
+
+    final colorListWidget = ValueListenableBuilder(
+      valueListenable: _selectedColorIndex,
+      builder: (context, value, _) {
+        return Row(
+          children: List.generate(_colors.length, (i) {
+            return Padding(
+                padding: const EdgeInsets.only(right: 10),
+                child: MyColorChoiceChip(
+                  selected: i == value,
+                  color: _colors[i],
+                  onPressed: () {
+                    _selectedColorIndex.value = i;
+                  },
+                ));
+          }),
+        );
+      },
+    );
+
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 18),
       child: Container(
@@ -19,28 +70,16 @@ class MyProductVariants extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text('Ukuran'),
-              SizedBox(height: 6),
-              Row(
-                children: [
-                  MyChoiceChip(),
-                  SizedBox(width: 10),
-                  MyChoiceChip(),
-                ],
-              ),
-              SizedBox(height: 12),
-              Text('Warna'),
-              SizedBox(height: 6),
-              Row(
-                children: [
-                  const MyColorChoiceChip(),
-                  SizedBox(width: 10),
-                  const MyColorChoiceChip(),
-                ],
-              ),
-              SizedBox(height: 12),
+              const Text('Ukuran'),
+              const SizedBox(height: 6),
+              sizeListWidget,
+              const SizedBox(height: 12),
+              const Text('Warna'),
+              const SizedBox(height: 6),
+              colorListWidget,
+              const SizedBox(height: 12),
               RichText(
-                text: TextSpan(
+                text: const TextSpan(
                   style: TextStyle(color: Colors.black),
                   children: [
                     TextSpan(text: 'Stok : '),
@@ -51,7 +90,7 @@ class MyProductVariants extends StatelessWidget {
                   ],
                 ),
               ),
-              SizedBox(height: 6),
+              const SizedBox(height: 6),
             ],
           ),
         ),

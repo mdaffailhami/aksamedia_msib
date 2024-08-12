f6206200bf5cc8bc3648531dc1fa855baea2d1d1
mdaffailhami <mdaffailhami@gmail.com>
2024-08-12 12:32:36 +0800

    Create intro.dart

---
+++ lib/screens/intro.dart
@@ -0,0 +1,43 @@
+import 'package:aksamedia_msib/components/intro_body.dart';
+import 'package:aksamedia_msib/components/intro_carousel.dart';
+import 'package:aksamedia_msib/constants.dart';
+import 'package:aksamedia_msib/widgets/filled_button.dart';
+import 'package:flutter/material.dart';
+
+class MyIntroScreen extends StatefulWidget {
+  const MyIntroScreen({super.key});
+
+  @override
+  State<MyIntroScreen> createState() => _MyIntroScreenState();
+}
+
+class _MyIntroScreenState extends State<MyIntroScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: double.infinity,
+        child: Stack(
+          children: [
+            const MyIntroCarousel(),
+            const MyIntroBody(),
+            Positioned(
+              top: 12,
+              right: 10,
+              child: TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Lewati',
+                  style: Theme.of(context)
+                      .textTheme
+                      .titleMedium
+                      ?.copyWith(color: const Color(0xFF434747)),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

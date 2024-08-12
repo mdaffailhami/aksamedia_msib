0adcd0b9f20c1bce2a91498b65be38b2b696ef0f
mdaffailhami <mdaffailhami@gmail.com>
2024-08-13 07:52:03 +0800

    Update choice_chip.dart

--- lib/widgets/choice_chip.dart
+++ lib/widgets/choice_chip.dart
@@ -1,18 +1,39 @@
+import 'package:aksamedia_msib/constants.dart';
 import 'package:flutter/material.dart';
 
 class MyChoiceChip extends StatelessWidget {
-  const MyChoiceChip({super.key});
+  const MyChoiceChip({
+    super.key,
+    required this.selected,
+    required this.text,
+    required this.onPressed,
+  });
+
+  final bool selected;
+  final String text;
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return ChoiceChip(
-      onSelected: (value) {},
-      label: Text('Paket 1'),
-      selected: false,
-      // selectedColor: Colors.transparent,
-      // bord
-      padding: EdgeInsets.symmetric(horizontal: 12),
+    const color = Color(0xFFE6E7E7);
+    return RawChip(
+      selected: selected,
+      label: Text(
+        text,
+        style: const TextStyle(fontWeight: FontWeight.bold),
+      ),
+      onPressed: () => onPressed(),
       showCheckmark: false,
+      padding: const EdgeInsets.symmetric(horizontal: 12),
+      backgroundColor: color,
+      selectedColor: color,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(8),
+        side: BorderSide(
+          width: 1.5,
+          color: selected ? primaryColor : color,
+        ),
+      ),
     );
   }
 }

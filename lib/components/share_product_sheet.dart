2667811760d7a3ee9261ed05a49957c8ddd8b084
mdaffailhami <mdaffailhami@gmail.com>
2024-08-13 07:51:14 +0800

    Create share_product_sheet.dart

---
+++ lib/components/share_product_sheet.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class MyShareProductSheet extends StatelessWidget {
+  const MyShareProductSheet({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomSheet(
+      onClosing: () {},
+      builder: (context) {
+        return Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Row(
+                children: [
+                  IconButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    icon: const Icon(
+                      Icons.close_rounded,
+                      size: 32,
+                    ),
+                  ),
+                  const SizedBox(width: 8),
+                  Text(
+                    'Bagikan Produk',
+                    style: Theme.of(context)
+                        .textTheme
+                        .titleMedium
+                        ?.copyWith(fontWeight: FontWeight.bold),
+                  ),
+                ],
+              ),
+              ListTile(
+                onTap: () {},
+                title: const Text('Teks dan Link'),
+              ),
+              const Divider(),
+              ListTile(
+                onTap: () {},
+                title: const Text('Gambar'),
+              ),
+              const Divider(),
+              const SizedBox(height: 80),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}

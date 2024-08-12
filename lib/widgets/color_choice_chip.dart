ed06a4a93939299eaf5f6fba9018d0203687ae43
mdaffailhami <mdaffailhami@gmail.com>
2024-08-13 07:52:07 +0800

    Update color_choice_chip.dart

--- lib/widgets/color_choice_chip.dart
+++ lib/widgets/color_choice_chip.dart
@@ -1,16 +1,32 @@
+import 'package:aksamedia_msib/constants.dart';
 import 'package:flutter/material.dart';
 
 class MyColorChoiceChip extends StatelessWidget {
-  const MyColorChoiceChip({super.key});
+  const MyColorChoiceChip({
+    super.key,
+    required this.selected,
+    required this.color,
+    required this.onPressed,
+  });
+
+  final bool selected;
+  final Color color;
+  final void Function() onPressed;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 44,
-      height: 44,
-      decoration: BoxDecoration(
-        color: Colors.red,
-        borderRadius: BorderRadius.circular(22),
+    const size = 44.0;
+
+    return InkWell(
+      onTap: () => onPressed(),
+      child: Container(
+        width: size,
+        height: size,
+        decoration: BoxDecoration(
+          color: color,
+          borderRadius: BorderRadius.circular(size / 2),
+          border: !selected ? null : Border.all(color: primaryColor, width: 2),
+        ),
       ),
     );
   }

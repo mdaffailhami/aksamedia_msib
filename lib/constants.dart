a1cf2face2f8285b0623812e9bb7beb548172c02
mdaffailhami <mdaffailhami@gmail.com>
2024-08-12 22:53:31 +0800

    Update constants.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 
-const primaryColor = Color(0xFF000000);
+const primaryColor = Color(0xFF242626);
 const secondaryColor = Color(0xFFECE806);
 const infoColor = Color(0xFF0EA5E9);
+const successColor = Color(0xFF16A34A);
